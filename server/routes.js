5102472b916bc01ecaf75e1d8b865654409e5e7c
freddinalcantara <freddyn.alcantara@gmail.com>
2017-10-07 17:27:51 -0400

    basic

---
+++ server/routes.js
@@ -0,0 +1,17 @@
+module.exports = (function(){
+'use-strict';
+const path          = require('path');
+
+var router = require('express').Router();
+
+router.get('/hello',function(request,response){
+    response.status(200).send("hello world!");
+});
+
+router.get('/',function(request,response){
+     response.sendFile(path.resolve('../public/assest/index.html'));
+})
+
+//always remenber to return a router to work!!
+return router;
+})();
---
+++ server/server.js
@@ -0,0 +1,37 @@
+// Initial package setups (ECMA 6 version)
+const http          = require("http");
+const express       = require("express");
+const bodyParser    = require("body-parser");
+const routes        = require("./routes.js");
+const config        = require("../config/config.json");
+const path          = require('path');
+
+
+const app = express();
+
+app.server = http.createServer(app);
+
+//set views to app
+app.set('views',path.resolve('../public/assest') );
+app.set('view engine', 'ejs');
+
+//set the static file for public use
+app.use(express.static('public'));
+
+app.use(bodyParser.json({
+    limit: config.bodyLimit,
+    type: config.bodyLimit
+}));
+
+app.use('/',routes);
+
+app.route('/config').get(function (req, res,nxt){
+    res.send(200).json(config);
+});
+
+const server = app.listen(config.port, function(){
+    var port = server.address().port;
+    var host = server.address().address;
+
+    console.log("%s server listening on port %s ...",host,port);
+});
